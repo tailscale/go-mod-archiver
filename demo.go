ac8405739000cb17c3302a1716bf3549fd670ba0
Brad Fitzpatrick <brad@danga.com>
2021-05-24 09:45:16 -0700

    Start of a go.mod vendor archiver to git.

---
+++ demo.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// No-op program, just to have a place to add imports to
+	// update go.mod.
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bradfitz/go-mod-archiver
+
+go 1.16
